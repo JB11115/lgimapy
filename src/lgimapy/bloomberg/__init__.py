4814edd8eb157f16a629a615264aa99fede04a39
JB11115 <jason.becker@lgima.com>
2019-06-11 16:21:54 -0500

    added coupon dates to init

--- src/lgimapy/bloomberg/__init__.py
+++ src/lgimapy/bloomberg/__init__.py
@@ -1,7 +1,18 @@
-from lgimapy.bloomberg.bdh import bdh
+from lgimapy.bloomberg.core import bdh, bdp, bds
+from lgimapy.bloomberg.coupon_dates import get_coupon_dates, scrape_coupon_dates
 from lgimapy.bloomberg.subsectors import (
-    get_bloomberg_subsector,
     update_subsector_json,
+    get_bloomberg_subsector,
+    scrape_bloomberg_subsectors,
 )
 
-__all__ = ["bdh", "get_bloomberg_subsector", "update_subsector_json"]
+__all__ = [
+    "bdh",
+    "bdp",
+    "bds",
+    "get_bloomberg_subsector",
+    "update_subsector_json",
+    "scrape_bloomberg_subsectors",
+    "get_coupon_dates",
+    "scrape_coupon_dates",
+]
