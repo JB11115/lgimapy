489a845a2f64f3304984cdaf2ac827f437cbd90f
JB11115 <jason.becker@lgima.com>
2019-07-29 14:09:45 -0500

    initial look at axioma curves

--- src/lgimapy/notebooks/axioma_curve_study.py
+++ src/lgimapy/notebooks/axioma_curve_study.py
@@ -2,6 +2,7 @@ from collections import defaultdict
 
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mtick
+import numpy as np
 import pandas as pd
 
 from lgimapy.index import IndexBuilder, Bond
@@ -15,9 +16,16 @@ def main():
     # %%
     # ------------------------------------------------------------------ #
     ticker = "VZ"  # ABIBB C F KMT VZ
-    df = read_csv(ticker)
-    date = df.index[-1]  # pick any date
+    spread_df = read_csv(f"USD-{ticker}-SEN_2019")
+    date = spread_df.index[-1]  # pick any date
     # ------------------------------------------------------------------ #
+
+    # Compute yield by adding swap curve to spread curve using
+    # linear interpolation of the swap curve.
+    swap_df = read_csv("USDswapCurve_2019")
+    yield_df = spread_df.loc[date, :] + np.interp(
+        spread_df.columns, swap_df.columns, swap_df.loc[date, :]
+    )
 
     ixb.load(date=date)
     ix = ixb.build(ticker=ticker)
@@ -29,37 +37,35 @@ def main():
         d["oas"].append(b.OAS)
 
     # %%
-    plot_yields_oas(ticker, date, d)
+    plot_yields_oas(ticker, date, d, spread_df, yield_df)
     plt.show()
     # %%
 
 
-def read_csv(issuer):
-    swap = pd.read_csv(
-        root(f"data/axioma_curves/USD-{issuer}-SEN_2019.csv"),
-        index_col=0,
-        parse_dates=True,
-        infer_datetime_format=True,
-    )
+def read_csv(fid):
     df = pd.read_csv(
-        root(f"data/axioma_curves/USD-{issuer}-SEN_2019.csv"),
+        root(f"data/axioma_curves/{fid}.csv"),
         index_col=0,
         parse_dates=True,
         infer_datetime_format=True,
     )
-    df += swap
     df.columns = [clean_col(c) for c in df.columns]
-    return df
+    return df[[c for c in df.columns if c <= 40]]
 
 
 def clean_col(col):
     if "Y" in col:
-        return float(col.strip("Y"))
+        c = float(col.strip("Y"))
     elif "M" in col:
-        return float(col.strip("M")) / 12
+        c = float(col.strip("M")) / 12
+    elif "W" in col:
+        c = float(col.strip("W")) / 52
+    elif "D" in col:
+        c = float(col.strip("D")) / 365
+    return round(c, 3)
 
 
-def plot_yields_oas(ticker, date, d):
+def plot_yields_oas(ticker, date, d, spread_df, yield_df):
     fig, axes = plt.subplots(2, 1, figsize=[14, 10])
     tick = mtick.StrMethodFormatter("{x:.2%}")
 
@@ -67,11 +73,11 @@ def plot_yields_oas(ticker, date, d):
         d["mats"], d["ytm"], "o", c="darkgreen", label="Market Value YTM", ms=4
     )
     axes[0].plot(
-        list(df),
-        df.loc[date, :],
+        yield_df.index,
+        yield_df.values,
         "--o",
         c="k",
-        label="Axioma Curve",
+        label="Axioma Issuer Yield Curve (Swap + Spread)",
         lw=1,
         ms=4,
     )
@@ -79,14 +85,12 @@ def plot_yields_oas(ticker, date, d):
     axes[0].set_ylabel("Yield")
     axes[0].yaxis.set_major_formatter(tick)
     axes[0].legend()
-    axes[0].set_xlim((-1, 2 + max(d["mats"])))
-
     axes[1].plot(
-        list(df),
-        df.loc[date, :],
+        list(spread_df),
+        spread_df.loc[date, :]
         "--o",
         c="k",
-        label="Axioma Curve",
+        label="Axioma Issuer Spread Curve",
         lw=1,
         ms=4,
     )
@@ -98,7 +102,6 @@ def plot_yields_oas(ticker, date, d):
     axes[1].set_ylabel("Yield")
     axes[1].yaxis.set_major_formatter(tick)
     axes[1].legend()
-    axes[1].set_xlim((-1, 2 + max(d["mats"])))
 
     fig.suptitle(f'{ticker} - {date.strftime("%m/%d/%Y")}', fontsize=20)
 
