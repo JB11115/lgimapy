c4316637045c8c150621bd10fb816690ee1b3842
JB11115 <jason.becker@lgima.com>
2019-08-27 15:58:31 -0500

    new module for identifying trade dates and holidays

---
+++ src/lgimapy/data/trade_dates.py
@@ -0,0 +1,70 @@
+import pandas as pd
+
+from lgimapy.data import Database
+from lgimapy.utils import root
+
+
+def update_trade_dates():
+    """
+    Update `trade_dates.csv` file with all trade dates
+    and holidays.
+    """
+    # Create DataFrame with trade date index and boolean value for holiday.
+    fid = root("data/trade_dates.csv")
+    trade_dates = Database().load_trade_dates()
+    df = pd.DataFrame(index=trade_dates)
+    df["holiday"] = 0
+
+    try:
+        saved_df = pd.read_csv(
+            fid, index_col=0, parse_dates=True, infer_datetime_format=True
+        )
+    except FileNotFoundError:
+        # No file, find all holidays.
+        holidays = find_holidays(trade_dates)
+    else:
+        # Succesfully loaded file, find holidays since last holiday.
+        saved_holidays = list(saved_df[saved_df["holiday"] == 1].index)
+        new_holidays = find_holidays(trade_dates, start_date=saved_holidays[-1])
+        holidays = list(set(saved_holidays + new_holidays))
+        if new_holidays:
+            # Print new holidays to the screen.
+            fout = [f"  {h.strftime('%m/%d/%Y')}" for h in new_holidays]
+            print("New Holidays:")
+            print(*fout, sep="\n")
+
+    # Set boolean value to 1 for holidays and save to .csv file.
+    df.loc[df.index.isin(holidays), "holiday"] = 1
+    df.to_csv(fid)
+    print("Updated Trade Dates")
+
+
+def find_holidays(trade_dates, start_date=None):
+    """List[datetime]: Find all holidays after given start date."""
+    db = Database()
+    last_date = trade_dates[-1]
+    start_date = "1/1/2004" if start_date is None else start_date
+    start_date = pd.to_datetime(start_date)
+    holidays = []
+    for year in range(start_date.year, last_date.year + 1):
+        # Load either full year or correct portion of the year.
+        start = f"12/31/{year-1}" if year != start_date.year else start_date
+        end = f"1/5/{year+1}" if year != last_date.year else last_date
+        db.load_market_data(start=start, end=end, local=True)
+        ix = db.build_market_index(in_returns_index=True)
+
+        # Find days where price, duration, and spread don't change
+        # for at least 100 cusips. These are assumed to be holidays.
+        # **On a non-holiday the number is never greater than 6.
+        cols = ["CleanPrice", "OAD", "OAS"]
+        dupe_locs = ix.df.duplicated(subset=cols, keep="first")
+        df = ix.df[dupe_locs]
+        counts = df["Date"].value_counts()
+        # Add new holidays to holiday list.
+        holidays.extend(list(counts[counts > 100].index))
+
+    return holidays
+
+
+if __name__ == "__main__":
+    update_trade_dates()
