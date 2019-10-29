4d454e5fa646df7f01f2222dac44abd984c8a1df
JB11115 <jason.becker@lgima.com>
2019-10-29 13:23:36 -0500

    fixed special cases with incorrect index

--- src/lgimapy/bloomberg/bloomberg_indexes.py
+++ src/lgimapy/bloomberg/bloomberg_indexes.py
@@ -31,13 +31,32 @@ def bloomberg_index_history(
     index_map = load_json("bloomberg_indexes")
     indexes = tolist(index, dtype=str)
 
-    field_map = {"TR": "TRUU", "TRET": "TRUU", "XSRET": "ER", "YTW": "YW"}
-    special_cases = {
+    field_map = {
+        "TR": "TRUU",
+        "TRET": "TRUU",
+        "XSRET": "ER",
+        "YTW": "YW",
+        "PR": "PRC",
+        "PRICE": "PRC",
+    }
+    security_special_cases = {
+        "LX01OAS": "CDX IG CDSI GEN 5Y",
+        "LX07OAS": "CDX HY CDSI GEN 5Y SPRD",
+        "LX07PRC": "CDX HY CDSI GEN 5Y PRC",
+        "LX10OAS": "CDX EM CDSI GEN 5Y SPRD",
+        "LX10PRC": "CDX EM CDSI GEN 5Y PRC",
         "LX01ER": "UISYMI5E",
         "LX07ER": "UISYMH5E",
         "CEMBTOOAS": "CEMBTOBS",
         "CEMBTOTRUU": "CEMBTOTR",
         "CEMBTOYW": "CEMBTOYI",
+    }
+    yellow_key_special_cases = {
+        "LX01OAS": "Corp",
+        "LX07OAS": "Corp",
+        "LX07PRC": "Corp",
+        "LX10OAS": "Corp",
+        "LX10PRC": "Corp",
     }
 
     df_list = []
@@ -47,10 +66,16 @@ def bloomberg_index_history(
         security, yellow_key = index_map[ix]
         if field is not None:
             security += field_map.get(field.upper(), field.upper())
-        security = special_cases.get(security, security).upper()
+        yellow_key = yellow_key_special_cases.get(security, yellow_key)
+        security = security_special_cases.get(security, security).upper()
+
         # Perform BDH call for each index.
-        df = bdh(security, yellow_key, bloomberg_field, start, end)
-        df.columns = [ix]
-        dfs.append(df)
+        try:
+            df = bdh(security, yellow_key, bloomberg_field, start, end)
+        except KeyError:
+            continue
+        else:
+            df.columns = [ix]
+            df_list.append(df)
 
     return pd.concat(df_list, join="outer", sort=False, axis=1)
