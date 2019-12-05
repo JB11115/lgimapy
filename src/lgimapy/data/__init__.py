8ed96cec3d22736ab4ac4f00861c249a7bbfa45d
JB11115 <jason.becker@lgima.com>
2019-12-05 14:30:42 -0600

    added update_bloomberg_data and update_dealer_inventory

--- src/lgimapy/data/__init__.py
+++ src/lgimapy/data/__init__.py
@@ -7,10 +7,11 @@ from lgimapy.data.security_functions import (
 from lgimapy.data.securities import Bond, SyntheticTBill, TBond, TreasuryCurve
 from lgimapy.data.index import Index
 from lgimapy.data.database import Database
+from lgimapy.data.bloomberg_data import update_bloomberg_data
 from lgimapy.data.fed_funds import update_fed_funds
 from lgimapy.data.index_feathers import update_feathers
 from lgimapy.data.trade_dates import update_trade_dates
-
+from lgimapy.data.dealer_inventory import update_dealer_inventory
 
 __all__ = [
     "concat_index_dfs",
@@ -23,7 +24,9 @@ __all__ = [
     "TreasuryCurve",
     "Index",
     "Database",
+    "update_bloomberg_data",
     "update_fed_funds",
     "update_feathers",
     "update_trade_dates",
+    "update_dealer_inventory",
 ]
