342520569dde0b8b9ce6a058324c3da99a1c1d19
JB11115 <jason.becker@lgima.com>
2019-12-05 14:34:02 -0600

    sped up finding trade dates using Database class

--- src/lgimapy/data/index_feathers.py
+++ src/lgimapy/data/index_feathers.py
@@ -3,13 +3,14 @@ from datetime import timedelta
 
 import numpy as np
 import pandas as pd
+from pyarrow import ArrowIOError
 from tqdm import tqdm
 
 from lgimapy.data import Database
 from lgimapy.utils import mkdir, root
 
 
-def create_feather(fid):
+def create_feather(fid, all_trade_dates):
     """
     Create feather files for all months in database.
 
@@ -17,6 +18,8 @@ def create_feather(fid):
     ----------
     fid: str
         Filename for new feather.
+    all_trade_dates:
+        List of all dates available in DataMart.
     """
     db = Database()
     dates = np.array(db.trade_dates)
@@ -30,7 +33,7 @@ def create_feather(fid):
             dates >= pd.to_datetime(f"{next_month}/1/{next_year}")
         ][0]
     except IndexError:
-        end_date = db.load_trade_dates()[-1]
+        end_date = all_trade_dates[-1]
     start_date = dates[dates < pd.to_datetime(f"{month}/1/{year}")][-1]
 
     # Load index, compute MTD excess returns, and save feather.
@@ -41,9 +44,14 @@ def create_feather(fid):
     df.to_feather(root(f"data/feathers/{fid}.feather"))
 
 
-def find_missing_feathers():
+def find_missing_feathers(all_trade_dates):
     """
     Find missing feathers from data dir.
+
+    Parameters
+    ----------
+    all_trade_dates:
+        List of all dates available in DataMart.
 
     Returns
     -------
@@ -61,43 +69,58 @@ def find_missing_feathers():
         mkdir(root("data/feathers"))
 
     all_feathers = pd.date_range(
-        "1/1/2004", Database().trade_dates[-1], freq="MS"
+        "2/1/1998", Database().trade_dates[-1], freq="MS"
     ).strftime("%Y_%m")
     missing_feathers = [f for f in all_feathers if f not in saved_feathers]
 
     # Check that all trade dates for past couple months are
     # in saved feather files.
-    # db = Database()
-    # start = dt.today() - timedelta(45)
-    # db.load_market_data(start=start, local=True)
-    # ix = db.build_market_index()
-    # saved_dates = ix.all_trade_dates
-    #
-    # trade_dates = db.load_trade_dates()[-30:]
-    # missing_dates = [date for date in trade_dates if date not in saved_dates]
-    # missing_dates
-    return missing_feathers
+    db = Database()
+    start = dt.today() - timedelta(45)
+    # Load local data, if local data doesn't exist create it.
+    while True:
+        try:
+            db.load_market_data(start=start, local=True)
+        except ArrowIOError as e:
+            missing = str(e).split(".feather")[0].rsplit("feathers\\", 1)[1]
+            create_feather(missing, all_trade_dates)
+        else:
+            break
+    ix = db.build_market_index()
+    saved_dates = ix.dates
+    # Compare to all dates in database to find missing dates.
+    dates_to_save = [
+        d
+        for d in all_trade_dates
+        if (d > saved_dates[0]) & (d not in db.holiday_dates)
+    ]
+    missing_dates = [
+        d.strftime("%Y_%m") for d in dates_to_save if d not in saved_dates
+    ]
+    missing_feathers.extend(missing_dates)
+    return sorted(list(set(missing_feathers)))
 
 
-def update_feathers():
+def update_feathers(dates=None):
     """
     Update feather files. Finds and creates any missing
     historical feathers and updates the feather for
-    current day.
+    current day. Creates verbose progress bar if more
+    than 2 files are to be created.
 
     Parameters
     ----------
-    verbose: bool, default=True
-        If True print updating message and display progress bar.
+    dates: List[datetime].
+        List of all trade dates available in DataMart.
     """
-    missing_fids = find_missing_feathers()
-    missing_fids.append(dt.today().strftime("%Y_%m"))
+    dates = Database().load_trade_dates() if dates is None else dates
+    missing_fids = find_missing_feathers(dates)
     pbar = len(missing_fids) > 2
     if pbar:
         print("Updating Feather Files")
     for fid in tqdm(missing_fids, disable=(not pbar)):
         try:
-            create_feather(fid)
+            create_feather(fid, dates)
         except (KeyboardInterrupt, SystemExit):
             raise
         except Exception as e:
