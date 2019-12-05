094f4d9c54443ff5b6625e95742ccd0bdb857798
JB11115 <jason.becker@lgima.com>
2019-12-05 14:26:01 -0600

    Added update_bloomberg_data to daily update

--- src/lgimapy/daily_scripts/daily_update.py
+++ src/lgimapy/daily_scripts/daily_update.py
@@ -1,11 +1,16 @@
 from time import sleep
 
-from lgimapy.daily_scripts import build_movers_sheets
+from lgimapy.daily_scripts import (
+    build_movers_sheets,
+    build_long_credit_snapshot,
+)
 from lgimapy.data import (
     Database,
+    update_bloomberg_data,
     update_feathers,
     update_fed_funds,
     update_trade_dates,
+    update_dealer_inventory,
 )
 from lgimapy.models import update_treasury_curve_dates
 
@@ -14,23 +19,41 @@ def main():
     """
     Run all daily scripts for updating database and creating reports.
     """
-    check_datamart_quality()
+    trade_dates = Database().load_trade_dates()
+    check_datamart_quality(trade_dates)
     print()
-    update_fed_funds()
+    update_fed_funds(trade_dates)
     print("Updated Fed Funds\n")
     update_treasury_curve_dates()
 
     update_feathers()
     print()
-    update_trade_dates()
+    update_trade_dates(trade_dates)
     print()
+    update_bloomberg_data()
+    print("Updated Bloomberg Data\n")
+    update_dealer_inventory()
+    print("Updated Dealer Inventory\n")
     build_movers_sheets()
     print("Index Mover Report Complete\n")
+    build_long_credit_snapshot()
+    print("Long Credit Snaphsot Complete\n")
 
 
-def check_datamart_quality():
+def check_datamart_quality(dates):
+    """
+    Verify DataMart quality by ensuring a minimum of 17,000
+    bonds are available for current day. If less than 17,000
+    are available, wait one minute and retry until more
+    than 17,000 are availble and the number has stabalized.
+
+    Parameters
+    ----------
+    dates: List[datetime].
+        List of all trade dates available in DataMart.
+    """
     db = Database()
-    date = db.load_trade_dates()[-1]
+    date = dates[-1]
     print(date.strftime("%m/%d/%Y"))
     db.load_market_data(date=date)
     ix = db.build_market_index()
@@ -44,8 +67,8 @@ def check_datamart_quality():
             break
         else:
             print(f"Currently only {n_bonds} populated, waiting...")
-            sleep(60)
-            db.load_market_data()
+            sleep(30)
+            db.load_market_data(date=date)
             ix = db.build_market_index()
             n_bonds_prev = n_bonds
             n_bonds = len(ix.df)
