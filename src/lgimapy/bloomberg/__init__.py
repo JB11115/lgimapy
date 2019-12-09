064c37a381fbb73ef322a381e9c0f8cc0b242a09
JB11115 <jason.becker@lgima.com>
2019-12-09 17:00:29 -0600

    Removed old bloomberg timeseries module

--- src/lgimapy/bloomberg/__init__.py
+++ src/lgimapy/bloomberg/__init__.py
@@ -5,7 +5,6 @@ from lgimapy.bloomberg.subsectors import (
     get_bloomberg_subsector,
     scrape_bloomberg_subsectors,
 )
-from lgimapy.bloomberg.bloomberg_indexes import bloomberg_index_history
 from lgimapy.bloomberg.cashflows import get_cashflows
 from lgimapy.bloomberg.issue_price import get_issue_price
 from lgimapy.bloomberg.cusip_ticker import get_bloomberg_ticker
@@ -21,7 +20,6 @@ __all__ = [
     "get_bloomberg_subsector",
     "update_subsector_json",
     "scrape_bloomberg_subsectors",
-    "bloomberg_index_history",
     "get_cashflows",
     "scrape_coupon_dates",
     "id_to_cusip",
