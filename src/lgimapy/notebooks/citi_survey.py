9b1a12a7521e253ac83fd57810f8e289c74b27c3
JB11115 <jason.becker@lgima.com>
2020-03-30 14:34:42 -0500

    updated DTS calculation

--- src/lgimapy/notebooks/citi_survey.py
+++ src/lgimapy/notebooks/citi_survey.py
@@ -15,9 +15,7 @@ db = Database()
 raw_kwargs = load_json("indexes")
 kwargs = {}
 for key, val in raw_kwargs.items():
-    kwargs[key] = {
-        k: val[k] for k in set(val.keys()) - set(["in_stats_index"])
-    }
+    kwargs[key] = {k: val[k] for k in set(val.keys()) - set(["in_stats_index"])}
 
 
 def get_position(oad=None, dts=None):
@@ -30,15 +28,15 @@ def get_position(oad=None, dts=None):
         levels = [1, 0.975, 0.945, 0.92]
 
     if val > levels[0]:
-        return '+2'
+        return "+2"
     elif val > levels[1]:
-        return '+1'
+        return "+1"
     elif val > levels[2]:
-        return '0'
+        return "0"
     elif val > levels[3]:
-        return '-1'
+        return "-1"
     else:
-        return '-2'
+        return "-2"
 
 
 # Get position of P-LD account sectors.
@@ -62,43 +60,43 @@ db.load_market_data(data=df)
 for sector in pld_sectors:
     ix = db.build_market_index(**kwargs[sector])
     oad = np.sum(ix.df["OAD_Diff"])
-    positions[kwargs[sector]['name']] = get_position(oad=oad)
+    positions[kwargs[sector]["name"]] = get_position(oad=oad)
 
 # Get positions for overall credit.
-dts = np.sum(df['P_DTS']) / np.sum(df['BM_DTS'])
-positions['Overall High Grade Corp'] = get_position(dts=dts)
-positions['Overall in Credit'] = get_position(dts=dts)
+dts = np.sum(df["P_DTS"]) / np.sum(df["BM_DTS"])
+positions["Overall High Grade Corp"] = get_position(dts=dts)
+positions["Overall in Credit"] = get_position(dts=dts)
 
 # Get position for ABS/CMBS.
 df = db.load_portfolio(accounts="P-MC", market_cols=True)
-ix = db.build_market_index(**kwargs['ABS_CMBS'])
+ix = db.build_market_index(**kwargs["ABS_CMBS"])
 oad = np.sum(ix.df["OAD_Diff"])
-positions[kwargs['ABS_CMBS']['name']] = get_position(oad=oad)
+positions[kwargs["ABS_CMBS"]["name"]] = get_position(oad=oad)
 
 # Add Nans.
-positions['Hybrids'] = 'N/A'
-positions['Overall High Yield'] = 'N/A'
-positions['Index/ETF Options'] = 'N/A'
+positions["Hybrids"] = "N/A"
+positions["Overall High Yield"] = "N/A"
+positions["Index/ETF Options"] = "N/A"
 
 # Print result.
 sorted_ix = [
-    'Telecom',
-    'Cyclical (Citi)',
-    'Consumer Non-Cyclical',
-    'Utilities',
-    'Industrials (Citi)',
-    'Energy',
-    'Banks (Sr)',
-    'Banks (Sub)',
-    'Emerging Markets',
-    'Hybrids',
-    'Insurance',
-    'Overall High Grade Corp',
-    'Overall High Yield',
-    'Index/ETF Options',
-    'Munis',
-    'ABS/CMBS/Non-Agency RMBS',
-    'Overall in Credit',
+    "Telecom",
+    "Cyclical (Citi)",
+    "Consumer Non-Cyclical",
+    "Utilities",
+    "Industrials (Citi)",
+    "Energy",
+    "Banks (Sr)",
+    "Banks (Sub)",
+    "Emerging Markets",
+    "Hybrids",
+    "Insurance",
+    "Overall High Grade Corp",
+    "Overall High Yield",
+    "Index/ETF Options",
+    "Munis",
+    "ABS/CMBS/Non-Agency RMBS",
+    "Overall in Credit",
 ]
 
 print(pd.Series(positions).reindex(sorted_ix))
