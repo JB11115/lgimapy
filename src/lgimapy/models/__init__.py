6ed55a37c37f3cc16c66cb5bef7bb4deca3b2506
JB11115 <jason.becker@lgima.com>
2020-03-30 14:31:45 -0500

    added drawdown model

--- src/lgimapy/models/__init__.py
+++ src/lgimapy/models/__init__.py
@@ -3,5 +3,12 @@ from lgimapy.models.treasury_curve import (
     update_treasury_curve_dates,
 )
 from lgimapy.models.total_least_squares import TLS
+from lgimapy.models.drawdown import find_drawdowns, plot_drawdown_timeseries
 
-__all__ = ["TreasuryCurveBuilder", "update_treasury_curve_dates", "TLS"]
+__all__ = [
+    "TreasuryCurveBuilder",
+    "update_treasury_curve_dates",
+    "TLS",
+    "find_drawdowns",
+    "plot_drawdown_timeseries",
+]
