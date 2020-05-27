c374f72d15890727941ec8a72a6e17c9fa55b7ca
JB11115 <jason.becker@lgima.com>
2020-05-27 11:16:40 -0500

    added new functions to create daily data files

--- src/lgimapy/data/__init__.py
+++ src/lgimapy/data/__init__.py
@@ -8,7 +8,11 @@ from lgimapy.data.securities import Bond, SyntheticTBill, TBond, TreasuryCurve
 from lgimapy.data.basket import BondBasket, groupby
 from lgimapy.data.index import Index
 from lgimapy.data.portfolios import Account, Strategy
-from lgimapy.data.database import Database
+from lgimapy.data.database import (
+    Database,
+    clean_dtypes,
+    convert_sectors_to_fin_flags,
+)
 from lgimapy.data.bloomberg_data import update_bloomberg_data
 from lgimapy.data.treasury_oad import update_treasury_oad_values
 from lgimapy.data.account_values import update_account_market_values
@@ -17,7 +21,10 @@ from lgimapy.data.index_feathers import update_feathers
 from lgimapy.data.trade_dates import update_trade_dates
 from lgimapy.data.dealer_inventory import update_dealer_inventory
 from lgimapy.data.update_top_30_tickers import update_top_30_tickers
-from lgimapy.data.lgima_sectors import save_lgima_sectors
+from lgimapy.data.lgima_sectors import save_lgima_sectors, update_lgima_sectors
+from lgimapy.data.nonfin_spreads_by_sub_rating import update_nonfin_spreads
+from lgimapy.data.strategy_overweights import update_strategy_overweights
+from lgimapy.data.rating_changes import update_rating_changes
 
 __all__ = [
     "concat_index_dfs",
@@ -34,6 +41,8 @@ __all__ = [
     "Account",
     "Strategy",
     "Database",
+    "clean_dtypes",
+    "convert_sectors_to_fin_flags",
     "update_bloomberg_data",
     "update_treasury_oad_values",
     "update_account_market_values",
@@ -43,4 +52,8 @@ __all__ = [
     "update_dealer_inventory",
     "update_top_30_tickers",
     "save_lgima_sectors",
+    "update_lgima_sectors",
+    "update_nonfin_spreads",
+    "update_strategy_overweights",
+    "update_rating_changes",
 ]
