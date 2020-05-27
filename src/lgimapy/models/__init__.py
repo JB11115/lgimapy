4c3f1c0273616b3a1cee9223681eb8f6d84f0b48
JB11115 <jason.becker@lgima.com>
2020-05-27 11:18:15 -0500

    added weighted percentile function

--- src/lgimapy/models/__init__.py
+++ src/lgimapy/models/__init__.py
@@ -1,14 +1,16 @@
-from lgimapy.models.treasury_curve import (
-    TreasuryCurveBuilder,
-    update_treasury_curve_dates,
-)
+# from lgimapy.models.treasury_curve import (
+# TreasuryCurveBuilder,
+# update_treasury_curve_dates,
+# )
 from lgimapy.models.total_least_squares import TLS
 from lgimapy.models.drawdown import find_drawdowns, plot_drawdown_timeseries
+from lgimapy.models.weighted_stats import weighted_percentile
 
 __all__ = [
-    "TreasuryCurveBuilder",
-    "update_treasury_curve_dates",
+    # "TreasuryCurveBuilder",
+    # "update_treasury_curve_dates",
     "TLS",
     "find_drawdowns",
     "plot_drawdown_timeseries",
+    "weighted_percentile",
 ]
