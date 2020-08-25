7b1cda0ec33635013b5d354a356670ac41c546aa
JB11115 <jason.becker@lgima.com>
2020-08-25 07:47:13 -0500

    corrected printed date

--- src/lgimapy/daily_scripts/index_marks.py
+++ src/lgimapy/daily_scripts/index_marks.py
@@ -1,11 +1,12 @@
 from lgimapy.data import Database
 
 db = Database()
-print(db.date("today").strftime("%m/%d/%Y"))
 
 ixs = {}
 ixs["Long Credit"] = db.load_bbg_data("US_IG_10+", "OAS", db.date("yesterday"))
 ixs["Market Credit"] = db.load_bbg_data("US_IG", "OAS", db.date("yesterday"))
+
+print(ixs["Long Credit"].index[-1].strftime("%m/%d/%Y"))
 
 for ix, df in ixs.items():
     diff = int(df.diff()[-1])
