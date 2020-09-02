c8bb287d41ae39359b4e116e80c92e685576cb33
JB11115 <jason.becker@lgima.com>
2020-09-02 08:09:19 -0500

    added command line arguments for specifying date and printing accounts

--- src/lgimapy/daily_scripts/performance.py
+++ src/lgimapy/daily_scripts/performance.py
@@ -1,3 +1,4 @@
+import argparse
 from pathlib import Path
 
 import pandas as pd
@@ -5,27 +6,54 @@ import pandas as pd
 from lgimapy.data import Database
 
 # %%
+def main():
+    args = parse_args()
+    accounts = [
+        "P-LD",
+        "FLD",
+        "GSKLC",
+        "CITMC",
+        "JOYLA",
+        "USBGC",
+    ]
+    if args.print:
+        for account in accounts:
+            print(account)
+        return
 
-path = Path('C:/blp/data')
-date = Database().date('today')
-print(date.strftime('%m/%d/%Y'))
-accounts = [
-    'P-LD',
-    'FLD',
-    'GSKLC',
-    'CITMC',
-    'JOYLA',
-    'USBGC',
-]
-for account in accounts:
-    fids = list(path.glob(f"*Daily*{account}.{date.strftime('%Y%m%d')}.xls"))
-    try:
-        fid = fids[0]
-    except IndexError:
-        raise FileNotFoundError(f'No file found for {account}.')
+    if args.date is not None:
+        date = pd.to_datetime(args.date)
+    else:
+        date = Database().date("today")
+    print(date.strftime("%m/%d/%Y"))
 
-    df = pd.read_excel(fid, usecols=[1, 2])
-    df.columns = ['ix', 'val']
-    df.set_index('ix', inplace=True)
-    performance = df.loc['Outperformance (bps)'].squeeze()
-    print(f"{account}: {performance:+.1f}")
+    path = Path("C:/blp/data")
+    for account in accounts:
+        fids = list(
+            path.glob(f"*Daily*{account}.{date.strftime('%Y%m%d')}.xls")
+        )
+        try:
+            fid = fids[0]
+        except IndexError:
+            raise FileNotFoundError(f"No file found for {account}.")
+
+        df = pd.read_excel(fid, usecols=[1, 2])
+        df.columns = ["ix", "val"]
+        df.set_index("ix", inplace=True)
+        performance = df.loc["Outperformance (bps)"].squeeze()
+        print(f"{account}: {performance:+.1f}")
+
+
+def parse_args():
+    """Collect settings from command line and set defaults."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--date", help="Performance Date")
+    parser.add_argument(
+        "-p", "--print", action="store_true", help="Print Accounts"
+    )
+    parser.set_defaults(date=None)
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    main()
