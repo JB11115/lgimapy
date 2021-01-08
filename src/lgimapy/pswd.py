fdfb2956ef31b2ce63982060dfd0f1023b9fcdf5
JB11115 <jason.becker@lgima.com>
2021-01-08 14:54:14 -0600

    added fuzzy searching for missing keys

--- src/lgimapy/pswd.py
+++ src/lgimapy/pswd.py
@@ -3,8 +3,9 @@ import json
 from pathlib import Path
 
 import pandas as pd
+from fuzzywuzzy import process
 
-
+# %%
 def main():
     args = parse_args()
     # Load passwords.
@@ -14,8 +15,21 @@ def main():
 
     # Print specified username and password.
     website = str(args.website).upper()
-    usr_pswd = pswd_d[website]
-    print(website)
+    try:
+        usr_pswd = pswd_d[website]
+    except KeyError:
+        website_names = get_all_website_names(pswd_d)
+        closest_matches = process.extract(
+            args.website, list(website_names.keys()), limit=5
+        )
+        print(f"Error. {website} is not present. Input number of correct key.")
+        for i, match in enumerate(closest_matches):
+            print(f"  {i}) {match[0]}")
+        int_key = int(input())
+        website_key = closest_matches[int_key][0]
+        pswd_key = website_names[website_key]
+        usr_pswd = pswd_d[pswd_key]
+
     if args.print:
         for key, val in usr_pswd.items():
             print(f"  {key}: {val}")
@@ -23,7 +37,7 @@ def main():
 
     # Add username, password, token, etc. to clipboard.
     for key, val in usr_pswd.items():
-        if key == "desc":
+        if key in {"desc", "name"}:
             continue
         try:
             pd.Series([val]).to_clipboard(index=False, header=False)
@@ -34,16 +48,25 @@ def main():
             input(f"Press any key to continue.")
 
 
+def get_all_website_names(d):
+    names = {}
+    for key, val in d.items():
+        for name in val["name"]:
+            names[name] = key
+    return names
+
+
 def parse_args():
     """Collect settings from command line and set defaults."""
     parser = argparse.ArgumentParser()
     parser.add_argument("website", help="Name of Website")
     parser.add_argument(
-        "-p", '--print', action='store_true', help="Print values."
+        "-p", "--print", action="store_true", help="Print values."
     )
 
     return parser.parse_args()
 
 
+# %%
 if __name__ == "__main__":
     main()
