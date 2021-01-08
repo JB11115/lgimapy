042fb938f9732f2e43f5f6c0f0fefd2c96f20edf
JB11115 <jason.becker@lgima.com>
2021-01-08 13:57:13 -0600

    changed to parquet file

--- src/lgimapy/data/account_values.py
+++ src/lgimapy/data/account_values.py
@@ -7,4 +7,4 @@ def update_account_market_values():
     Update account values for each strategy.
     """
     df = Database().account_values()
-    df.to_parquet(root('data/account_values.parquet'))
+    df.to_parquet(root("data/account_values.parquet"))
