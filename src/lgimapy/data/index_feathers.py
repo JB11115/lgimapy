fcb3bb7ee3c4ee254085c4e349314e33d4cd5b76
JB11115 <jason.becker@lgima.com>
2021-01-08 13:58:54 -0600

    save feathers for all markets

--- src/lgimapy/data/index_feathers.py
+++ src/lgimapy/data/index_feathers.py
@@ -1,5 +1,4 @@
-from datetime import datetime as dt
-from datetime import timedelta
+from dateutil.relativedelta import relativedelta
 
 import numpy as np
 import pandas as pd
@@ -7,16 +6,15 @@ from pyarrow import ArrowIOError
 from tqdm import tqdm
 
 from lgimapy.data import Database
-from lgimapy.utils import mkdir, root
+from lgimapy.utils import mkdir, root, restart_program
 
 
 # %%
-fid = "2020_05"
 
 
-def create_feather(fid, all_trade_dates):
+def create_feather(fid, db):
     """
-    Create feather files for all months in database.
+    Create feather files specified month from SQL.
 
     Parameters
     ----------
@@ -25,39 +23,41 @@ def create_feather(fid, all_trade_dates):
     all_trade_dates:
         List of all dates available in DataMart.
     """
-    db = Database()
-
     # Find start and end date for feather.
-    year, month = (int(d) for d in fid.split("_"))
-    next_month = month + 1 if month != 12 else 1
-    next_year = year if month != 12 else year + 1
+    first_of_month = pd.to_datetime(fid, format="%Y_%m")
+    last_of_month = first_of_month + relativedelta(day=31)
     try:
-        start = pd.to_datetime(f"{next_month}/1/{next_year}")
-        end_date = db.trade_dates(start=start)[0]
+        prev_last_of_month = db.trade_dates(exclusive_end=first_of_month)[-1]
     except IndexError:
-        end_date = all_trade_dates[-1]
-    ex_end = pd.to_datetime(f"{month}/1/{year}")
-    start_date = db.trade_dates(exclusive_end=ex_end)[-1]
+        prev_last_of_month = db.trade_dates()[0]
 
     # Load index and drop holidays.
-    db.load_market_data(start=start_date, end=end_date)
+    db.load_market_data(
+        start=prev_last_of_month, end=last_of_month, local=False
+    )
     ix = db.build_market_index()
     ix.df = ix.df[ix.df["Date"].isin(db.trade_dates())]
 
-    # Fill missing data with bloomberg data pre Oct-2018.
-    if start_date < pd.to_datetime("11/1/2018"):
-        ix._fill_missing_columns_with_bbg_data()
     # Add previous market value column.
     ix._get_prev_market_value_history()
-    # Compute MTD total and excess returns.
-    ix.compute_excess_returns()
 
-    # Save feather file.
-    df = ix.subset(start=f"{month}/1/{year}").df.reset_index(drop=True)
-    df.to_feather(root(f"data/feathers/{fid}.feather"))
+    if db.market == "US":
+        # Fill missing data with bloomberg data pre Oct-2018.
+        if first_of_month < pd.to_datetime("11/1/2018"):
+            ix._fill_missing_columns_with_bbg_data()
+
+        # Add HY index Flags.
+        ix._add_hy_index_flags()
+
+        # Compute MTD total and excess returns.
+        ix.compute_excess_returns()
+
+    # Remove previous month's last day and save feather file.
+    df = ix.subset(start=ix.dates[1]).df.reset_index(drop=True)
+    df.to_feather(root(f"data/{db.market}/feathers/{fid}.feather"))
 
 
-def find_missing_feathers(all_trade_dates):
+def find_missing_feathers(db):
     """
     Find missing feathers from data dir.
 
@@ -72,77 +72,61 @@ def find_missing_feathers(all_trade_dates):
         List of feather files which are not saved in the
         data directory.
     """
-    # Compare saved feather files to all feathers that should exist.
-    saved_feathers = [
-        f.name.strip(".feather")
-        for f in root("data/feathers").glob("*.feather")
-    ]
-    if not saved_feathers:
-        mkdir(root("data/feathers"))
+    # Compare saved feather files to feather files expected to exist.
+    feather_dir = root(f"data/{db.market}/feathers")
+    mkdir(feather_dir)
+    saved_feathers = sorted(feather_dir.glob("*.feather"))
+    saved_fids = set([fid.name.strip(".feather") for fid in saved_feathers])
+    expected_fids = set(
+        pd.date_range(
+            db.date("market_start").replace(day=1), db.date("today"), freq="MS"
+        ).strftime("%Y_%m")
+    )
+    missing_fids = expected_fids - saved_fids
 
-    all_feathers = pd.date_range(
-        "2/1/1998", Database().date("today"), freq="MS"
-    ).strftime("%Y_%m")
-    missing_feathers = [f for f in all_feathers if f not in saved_feathers]
+    # Check that all expected trade dates for past couple months are
+    # in saved the feather files.
+    try:
+        recent_df = pd.concat(
+            (pd.read_feather(fid) for fid in saved_feathers[-2:])
+        )
+    except ValueError:
+        return sorted(missing_fids)
+    else:
+        saved_dates = sorted(pd.to_datetime(recent_df["Date"].unique()))
+    expected_dates = db.trade_dates(start=saved_dates[0])
+    missing_dates = set(expected_dates) - set(saved_dates)
+    missing_date_fids = set([d.strftime("%Y_%m") for d in missing_dates])
 
-    # Check that all trade dates for past couple months are
-    # in saved feather files.
-    db = Database()
-    start = dt.today() - timedelta(45)
-    # Load local data, if local data doesn't exist create it.
-    while True:
-        try:
-            db.load_market_data(start=start, local=True)
-        except (FileNotFoundError, ArrowIOError) as e:
-            missing = str(e).split(".feather")[0].rsplit("feathers/", 1)[1]
-            create_feather(missing, all_trade_dates)
-        else:
-            break
-
-    ix = db.build_market_index()
-    saved_dates = ix.dates
-    # Compare to all dates in database to find missing dates.
-    dates_to_save = [
-        d
-        for d in all_trade_dates
-        if (d > saved_dates[0]) & (d not in db.holiday_dates)
-    ]
-    missing_dates = [
-        d.strftime("%Y_%m") for d in dates_to_save if d not in saved_dates
-    ]
-    missing_feathers.extend(missing_dates)
-    return sorted(list(set(missing_feathers)))
+    return sorted(missing_fids | missing_date_fids)
 
 
-def update_feathers(dates=None):
+def update_market_data_feathers(limit=20):
     """
-    Update feather files. Finds and creates any missing
-    historical feathers and updates the feather for
-    current day. Creates verbose progress bar if more
+    Update market data feather files for each market.
+    Finds and creates any missing feather files.
+    Creates verbose progress bar if more
     than 2 files are to be created.
-
-    Parameters
-    ----------
-    dates: List[datetime].
-        List of all trade dates available in DataMart.
     """
-    dates = Database().load_trade_dates() if dates is None else dates
-    missing_fids = find_missing_feathers(dates)
-    pbar = len(missing_fids) > 2
-    if pbar:
-        print("Updating Feather Files")
-    for fid in tqdm(missing_fids, disable=(not pbar)):
-        try:
-            create_feather(fid, dates)
-        except (KeyboardInterrupt, SystemExit):
-            raise
-        except Exception as e:
-            print(fid, "\n", e)
-            continue
-    print("Updated Feather Files")
+    markets = ["US", "GBP", "EUR"]
+    for market in markets:
+        db = Database(market=market)
+        missing_fids = find_missing_feathers(db)
+        pbar = len(missing_fids) > 3
+        if pbar:
+            print(f"Updating {market} Feather Files")
+        for fid in tqdm(missing_fids, disable=(not pbar)):
+            try:
+                create_feather(fid, db)
+            except (KeyboardInterrupt, SystemExit):
+                raise
+            except Exception as e:
+                print(fid, "\n", e)
+                continue
+        print(f"Updated {market} Feather Files")
 
+
+# %%
 
 if __name__ == "__main__":
-    dates = None
-    all_trade_dates = dates
-    update_feathers()
+    update_market_data_feathers()
