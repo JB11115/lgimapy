416c790add91bc0ae708d31b1d3029b159b53bbc
JB11115 <jason.becker@lgima.com>
2021-02-11 14:46:57 -0600

    initial commit morning call speaker order

---
+++ src/lgimapy/daily_scripts/morning_call.py
@@ -0,0 +1,28 @@
+speakers = """
+    -----------------------
+       *START RECORDING*
+    -----------------------
+    US Econ (Tim)
+    Global Macro (Ben)
+    EM (Woody/Raza/Tracy)
+    HY (Elizabeth/Oh-Me)
+    -----------------------
+    Rates
+    Credit Macro
+    -----------------------
+    Credit Trading
+    -----------------------
+    Banks
+    Insurance/REITs
+    Energy
+    Basics
+    Industrials/TMT
+    Healthcare/Consumer
+    Utilities
+    -----------------------
+    John
+    -----------------------
+    """
+
+for speaker in speakers.splitlines():
+    print(speaker)
