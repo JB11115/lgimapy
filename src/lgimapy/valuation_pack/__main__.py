f787bf16da96dae8ffc838e1d2602ca15181ae20
JB11115 <jason.becker@lgima.com>
2021-04-07 09:22:28 -0500

    main path location a variable to be passed to other functions

--- src/lgimapy/valuation_pack/__main__.py
+++ src/lgimapy/valuation_pack/__main__.py
@@ -13,18 +13,20 @@ from tail_recovery import update_lc_tail
 from macro_indicators import update_macro_indicators
 from equilibium_pca_model import update_equilibrium_model
 from sector_vol import update_sector_models
+from fallen_angel_analysis import update_fallen_angel_analysis
 
 # %%
 def main():
     # Duplicate template file and rename to todays date.
     fid = f"{dt.today().strftime('%Y-%m-%d')}_Valuation_Pack"
-    directory = root("reports/valuation_pack")
+    path = "reports/valuation_pack"
+    directory = root(path)
     src = directory / "template.tex"
     dst = directory / f"{fid}.tex"
     copy(src, dst)
 
     vis.style()
-    doc = Document(fid, path="reports/valuation_pack", fig_dir=True)
+    doc = Document(fid, path=path, fig_dir=True)
     db = Database()
     db.load_market_data(local=True, start=db.date("5y"))
     update_cover_page(fid, db)
@@ -33,11 +35,10 @@ def main():
     update_macro_indicators(fid)
     update_equilibrium_model(fid)
     update_sector_models(fid, db)
-    doc = Document(
-        fid, path="reports/valuation_pack", fig_dir=True, load_tex=True
-    )
-    doc.save(save_tex=True)
-    # os.remove(dst)
+    update_fallen_angel_analysis(fid)
+
+    doc = Document(fid, path=path, fig_dir=True, load_tex=True)
+    doc.save()
 
 
 if __name__ == "__main__":
