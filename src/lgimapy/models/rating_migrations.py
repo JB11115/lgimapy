74b1c3c75a793cf7f7792ec3a63adf653d2b73df
JB11115 <jason.becker@lgima.com>
2021-04-07 09:19:14 -0500

    moved rating migration functions to models dir

--- src/lgimapy/models/rating_migrations.py
+++ src/lgimapy/models/rating_migrations.py
@@ -1,156 +1,193 @@
-from collections import defaultdict
+import itertools as it
 
 import numpy as np
-import pandas as pd
 
 from lgimapy.bloomberg import bdp
-from lgimapy.data import Database, Index
-from lgimapy.latex import Document
+from lgimapy.data import Database
+from lgimapy.utils import to_list
 
 # %%
-def get_fallen_angel_impact():
-    db = Database()
-    db.load_market_data()
-
-    sectors = [
-        "CHEMICALS",
-        "METALS_AND_MINING",
-        "COMMUNICATIONS",
-        "AUTOMOTIVE",
-        "RETAILERS",
-        "CONSUMER_CYCLICAL_EX_AUTOS_RETAILERS",
-        "HEALTHCARE_PHARMA",
-        "FOOD_AND_BEVERAGE",
-        "CONSUMER_NON_CYCLICAL_OTHER",
-        "ENERGY",
-        "TECHNOLOGY",
-        "TRANSPORTATION",
-        "BANKS",
-        "INSURANCE",
-        "REITS",
-        "UTILITY",
-    ]
-    ig_ix = db.build_market_index(in_stats_index=True)
-    bbb_ix = ig_ix.subset(rating=("BBB", None))
-    near_fa_ix = get_near_fallen_angels(bbb_ix)
-    hy_ix = db.build_market_index(in_H0A0_index=True)
-
-    ig_mv = ig_ix.total_value().iloc[0]
-    hy_mv = hy_ix.total_value().iloc[0]
-
-    d = defaultdict(list)
-    for sector in sectors:
-        kwargs = db.index_kwargs(
-            sector, unused_constraints=["in_stats_index", "OAS"]
-        )
-        # Find value of sector that is IG near a fallen angel.
-        near_fa_sector = near_fa_ix.subset(**kwargs)
-        if len(near_fa_sector.df):
-            near_fa_sector_mv = near_fa_sector.total_value().iloc[0]
-        else:
-            near_fa_sector_mv = 0
-        # Find value of sector that is HY.
-        hy_sector = hy_ix.subset(**kwargs)
-        if len(hy_sector.df):
-            hy_sector_mv = hy_sector.total_value().iloc[0]
-            potential_increase = near_fa_sector_mv / hy_sector_mv
-        else:
-            hy_sector_mv = 0
-            potential_increase = np.inf
-        d["Sector"].append(kwargs["name"])
-        d["PFA MV*(\\$B)"].append(near_fa_sector_mv / 1e3)
-        d["PFA*% of IG"].append(near_fa_sector_mv / ig_mv)
-        d["HY MV*(\\$B)"].append(hy_sector_mv / 1e3)
-        d["Sector*% of HY"].append(hy_sector_mv / hy_mv)
-        d["Potential*Increase"].append(potential_increase)
-
-    return pd.DataFrame(d)
 
 
-# %%
-ix = bbb_ix.copy()
-
-
-def get_near_fallen_angels(ix):
+def add_rating_outlooks(ix):
     """
-    Scrape rating outlooks from the major agencies and subset
-    :class:`Index` to bonds that are on negative outlook at a
-    rating agency where a downgrade from said agency would
-    result in a transition to the HY index.
+    Add the current outlook and current numeric rating for each
+    rating agency in columns to :attr:`Index.df`.
+
+    Parameters
+    ----------
+    ix: :class:`Index`
+        Input index.
+
+    Returns
+    -------
+    :class:`Index`:
+        Index with rating outlook and individual numberic rating
+        columns added for each rating agency.
     """
-    # %%
     db = Database()
     rating_agencies = ["SP", "Moody", "Fitch"]
+    outlook_map = {"NEG": 1, "POS": -1, "STABLE": 0, np.nan: 0}
+
     for agency in rating_agencies:
         bbg_agency = {"Moody": "MDY"}.get(agency, agency).upper()
         outlook = bdp(ix.cusips, "Corp", f"RTG_{bbg_agency}_OUTLOOK").squeeze()
-        ix.df[f"{agency}Outlook"] = (
-            outlook.str.contains("NEG").fillna(0).astype(int)
-        )
+        ix.df[f"{agency}Outlook"] = outlook.map(outlook_map)
         ix.df[f"{agency}NumericRating"] = db._get_numeric_ratings(
             ix.df, [f"{agency}Rating"]
         )
+    return ix
 
+
+def compare(s, migration, numeric_thresh):
+    if migration == "upgrade":
+        return s < numeric_thresh
+    elif migration == "downgrade":
+        return s > numeric_thresh
+
+
+def simulate_rating_migrations(
+    ix,
+    migration,
+    threshold,
+    notches,
+    max_individual_agency_notches=2,
+    new_column_name="{}Notch",
+    add_rating_outlook_columns=True,
+):
+    """
+    Simulate rating migrations from rating agencies using
+    current outlooks. Create a column for each number of notches
+    that would result in a downgrade past specified threshold.
+
+    Parameters
+    ----------
+    ix: :class:`Index`
+        Index to perform analysis on.
+    threshold: str or int
+        Rating or numeric rating for minimum (downgrade) or maximum
+        (upgrade) allowable rating.
+    migration ``{"upgrade", 'downgrade'}``
+        Whether to find potential upgrades or downgrades.
+    notches: int or List[int]:
+        Number of notches of rating migrations to apply.
+        Each notch results in its own column being added.
+    max_individual_agency_notches: int, default=2
+        Maximum number of notches for an individual rating agency
+        to be simulated to upgrade/downgrade.
+    new_column_name: str, default="{}Notch"
+        New column name for results. the {} is formatted with
+        the number of notches.
+    add_rating_outlook_columns: bool, default=True
+        If ``True``, add rating outlook columns using a Bloomberg
+        scrape.
+
+    Returns
+    -------
+    :class:`Index`:
+        Index with boolean columns for each set of notches
+        that result in a rating migration past given threshold.
+    """
+    db = Database()
+    if add_rating_outlook_columns:
+        ix = add_rating_outlooks(ix)
+
+    rating_agencies = ["SP", "Moody", "Fitch"]
     rating_cols = [f"{agency}NumericRating" for agency in rating_agencies]
+    outlook_cols = [f"{agency}Outlook" for agency in rating_agencies]
+    new_rating_cols = [f"{agency}New" for agency in rating_agencies]
 
-    # %%
-    potential_fa_d = defaultdict(lambda: np.zeros(len(ix.df), dtype=bool))
-    for i, ratings in ix.df[rating_cols].reset_index(drop=True).iterrows():
-        sorted_ratings = ratings.sort_values().dropna()
-        # Find bonds that are one downgrade away from becoming HY.
-        # - rated by one agency and is BBB-
-        # - rated by two agencies and the lower is BBB-
-        # - rated by three agencies and the middle is BBB-
-        if (
-            (len(sorted_ratings) == 1 and sorted_ratings.iloc[0] == 10)
-            or (len(sorted_ratings) == 2 and sorted_ratings.iloc[-1] == 10)
-            or (len(sorted_ratings) == 3 and sorted_ratings.iloc[1] == 10)
-        ):
-            for agency_col, rating in sorted_ratings.items():
-                if rating == 10:
-                    # Agency has BBB- rating.
-                    agency = agency_col.split("Num")[0]
-                    if ix.df[f"{agency}Outlook"].iloc[i]:
-                        # Agency has a negative outlook.
-                        is_near_fa[i] = 1
+    df = ix.df[rating_cols + outlook_cols].copy()
 
-    near_fa_df = ix.df.loc[is_near_fa.astype(bool)].copy()
-    return Index(near_fa_df)
+    # Focus only on upgrades or downgrades.
+    for col in outlook_cols:
+        if migration == "downgrade":
+            loc = df[col] < 0
+        elif migration == "upgrade":
+            loc = df[col] > 0
+        df[col] = np.where(loc, 0, df[col])
 
+    thresh = db.convert_input_ratings(threshold)[0]
 
-# %%
+    for notch in to_list(notches):
+        breaks_threshold = np.zeros(len(df))
+        if notch == 1:
+            # One agency does 1 notch downgrade.
+            for agency in rating_agencies:
+                # Perform 1 notch action for selected agency.
+                df[f"{agency}New"] = (
+                    df[f"{agency}NumericRating"] + df[f"{agency}Outlook"]
+                )
+                # Leave the other two agency ratings unchanged.
+                unchanged_agencies = set(rating_agencies) - set([agency])
+                for agency in unchanged_agencies:
+                    df[f"{agency}New"] = df[f"{agency}NumericRating"].values
+                # Store bonds which exceeded threshold.
+                new_rating = db._get_numeric_ratings(df, new_rating_cols)
+                loc = compare(new_rating, migration, thresh)
+                breaks_threshold[loc] = 1
 
+        elif notch == 2:
+            # Two agencies each perform a 1 notch action.
+            for action_agencies in it.combinations(rating_agencies, 2):
+                # Perform 1 notch downgrade for each action agency.
+                for agency in action_agencies:
+                    df[f"{agency}New"] = (
+                        df[f"{agency}NumericRating"] + df[f"{agency}Outlook"]
+                    )
+                # Leave the other agency unchanged.
+                agency = list(set(rating_agencies) - set(action_agencies))[0]
+                df[f"{agency}New"] = df[f"{agency}NumericRating"].values
+                # Store bonds which exceeded threshold.
+                new_rating = db._get_numeric_ratings(df, new_rating_cols)
+                loc = compare(new_rating, migration, thresh)
+                breaks_threshold[loc] = 1
 
-def main():
-    df = get_fallen_angel_impact()
-    # %%
-    fid = "Potential_Fallen_Angel_Impact"
-    doc = Document(fid, path="reports/HY")
-    prec = {col: "1%" for col in df.columns[1:]}
-    for key in prec.keys():
-        if "$" in key:
-            prec[key] = "0f"
-    cap = "Potential Fallen Angel Impact"
-    footnote = """
-        Potential fallen angel (PFA) for the IG index is defined as
-        any issuer that currently has a negative outlook from a
-        rating agency where a downgrade from said agency
-        would cause the issuer to become a fallen angel.
-        """
-    # edit = doc.add_subfigures(1, widths=[0.75])
-    # with doc.start_edit(edit):
-    doc.add_table(
-        df.sort_values("Potential*Increase", ascending=False),
-        caption=cap,
-        table_notes=footnote,
-        table_notes_justification="l",
-        adjust=True,
-        hide_index=True,
-        multi_row_header=True,
-        col_fmt="ll|cr|cr|r",
-        prec=prec,
-        gradient_cell_col="Potential*Increase",
-        gradient_cell_kws={"vmax": 1},
-    )
+            if max_individual_agency_notches >= 2:
+                # One agency does a 2 notch action.
+                for agency in rating_agencies:
+                    # Perform 2 notch action for selected agency.
+                    df[f"{agency}New"] = df[f"{agency}NumericRating"] + (
+                        2 * df[f"{agency}Outlook"]
+                    )
+                    # Leave the other two agency ratings unchanged.
+                    unchanged_agencies = set(rating_agencies) - set([agency])
+                    for agency in unchanged_agencies:
+                        df[f"{agency}New"] = df[f"{agency}NumericRating"].values
+                    # Store bonds which exceeded threshold.
+                    new_rating = db._get_numeric_ratings(df, new_rating_cols)
+                    loc = compare(new_rating, migration, thresh)
+                    breaks_threshold[loc] = 1
 
-    doc.save()
+        elif notch == 3:
+            # Each rating agency takes a 1 notch action.
+            for agency in rating_agencies:
+                df[f"{agency}New"] = (
+                    df[f"{agency}NumericRating"] + df[f"{agency}Outlook"]
+                )
+            # Store bonds which exceeded threshold.
+            new_rating = db._get_numeric_ratings(df, new_rating_cols)
+            loc = compare(new_rating, migration, thresh)
+            breaks_threshold[loc] = 1
+
+            if max_individual_agency_notches >= 2:
+                # One Rating agency takes a 2 notch
+                # action and one takes 1 notch.
+                for agencies in it.permutations(rating_agencies, 3):
+                    # With each possible permutation, assign a 0 notch
+                    # action to the first agency, 1 notch to the second,
+                    # and 2 notches to the third.
+                    for n_notches, agency in enumerate(agencies):
+                        df[f"{agency}New"] = df[f"{agency}NumericRating"] + (
+                            n_notches * df[f"{agency}Outlook"]
+                        )
+                        # Store bonds which exceeded threshold.
+                        new_rating = db._get_numeric_ratings(
+                            df, new_rating_cols
+                        )
+                        loc = compare(new_rating, migration, thresh)
+                        breaks_threshold[loc] = 1
+
+        ix.df[new_column_name.format(notch)] = breaks_threshold
+
+    return ix
