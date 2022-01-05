86bf23f91fa25b53a1b55ac3f888f86d0c2ef115
JB11115 <jason.becker@lgima.com>
2022-01-05 11:29:25 -0600

    added decimal places to output.

--- src/lgimapy/daily_scripts/index_marks.py
+++ src/lgimapy/daily_scripts/index_marks.py
@@ -9,10 +9,10 @@ ixs["Market Credit"] = db.load_bbg_data("US_IG", "OAS", db.date("yesterday"))
 print(ixs["Long Credit"].index[-1].strftime("%m/%d/%Y"))
 
 for ix, df in ixs.items():
-    diff = int(df.diff()[-1])
+    diff = df.diff().iloc[-1]
     if diff > 0:
-        print(f"  {ix}: {df.iloc[-1]:.0f}, {diff} bp wider")
+        print(f"  {ix}: {df.iloc[-1]:.1f}, {diff:.2f} bp wider")
     elif diff < 0:
-        print(f"  {ix}: {df.iloc[-1]:.0f}, {-diff} bp tighter")
+        print(f"  {ix}: {df.iloc[-1]:.1f}, {-diff:.2f} bp tighter")
     else:
-        print(f"  {ix}: {df.iloc[-1]:.0f}, unched")
+        print(f"  {ix}: {df.iloc[-1]:.2f}, unched")
