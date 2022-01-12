404c67ce13df0dcb8f2d29a5e08c6917ba676049
JB11115 <jason.becker@lgima.com>
2022-01-12 16:00:02 -0600

    Initial commit for stats library.

---
+++ src/lgimapy/stats/__init__.py
@@ -0,0 +1,32 @@
+from lgimapy.stats.stats import mode
+from lgimapy.stats.weighted_stats import (
+    mean,
+    var,
+    std,
+    RSD,
+    percentile,
+    median,
+    IDR,
+    DCV,
+    IQR,
+    QCV,
+    MAD,
+    RMAD,
+)
+
+
+__all__ = [
+    "mode",
+    "mean",
+    "var",
+    "std",
+    "RSD",
+    "percentile",
+    "median",
+    "IDR",
+    "DCV",
+    "IQR",
+    "QCV",
+    "MAD",
+    "RMAD",
+]
