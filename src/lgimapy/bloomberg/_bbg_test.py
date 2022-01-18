6bffdf23a6553035c079a279f32940cb0ff53343
JB11115 <jason.becker@lgima.com>
2022-01-18 17:52:12 -0600

    updated test to include bdh().

--- src/lgimapy/bloomberg/_bbg_test.py
+++ src/lgimapy/bloomberg/_bbg_test.py
@@ -2,19 +2,21 @@ from lgimapy.bloomberg import bdp, bdh, bds
 
 # %%
 
+
 def main():
-    cusip = '097023BV6'
-    yellow_key = 'Corp'
+    cusip = "097023BV6"
+    yellow_key = "Corp"
     ovrd = {"USER_LOCAL_TRADE_DATE": "19501010"}
 
-    print('testing bdp()...')
-    print(bdp(cusip, yellow_key, 'PX_LAST'))
+    print("testing bdp()...")
+    print(bdp(cusip, yellow_key, "PX_LAST"))
 
-    print('testing bdh()...')
-    print(bdp(cusip, yellow_key, 'PX_LAST', start='1/1/2022', end='1/10/2022'))
+    print("testing bdh()...")
+    print(bdh(cusip, yellow_key, "PX_LAST", start="1/1/2022", end="1/10/2022"))
 
-    print('testing bds()...')
-    print(bds(cusip, yellow_key, 'DES_CASH_FLOW', ovrd=ovrd))
+    print("testing bds()...")
+    print(bds(cusip, yellow_key, "DES_CASH_FLOW", ovrd=ovrd))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
