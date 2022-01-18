621b405318dbd5b53b84d7ea3658c26ef0adbf8e
JB11115 <jason.becker@lgima.com>
2022-01-18 13:23:56 -0600

    added function for editing the valuation pack .tex file inplace.

--- src/lgimapy/valuation_pack/dispersion.py
+++ src/lgimapy/valuation_pack/dispersion.py
@@ -20,15 +20,13 @@ def update_dispersion(fid):
     doc = Document(
         fid, path="reports/valuation_pack", fig_dir=True, load_tex=True
     )
-    with doc.start_edit("dispersion_page"):
-        add_dispersion_tables(doc, disp)
-
+    doc = edit_dispersion_tables(doc, disp)
     doc.save_tex()
 
     # %%
 
 
-def build_dispersion_sheet(doc, disp):
+def build_dispersion_sheet(fid, disp):
     date = db.date("today")
     doc = Document(fid, path="reports/valuation_pack", fig_dir=True)
     doc.add_preamble(
@@ -76,10 +74,44 @@ def add_dispersion_tables(doc, disp):
                 ],
                 gradient_cell_kws={"vmin": 0, "vmax": 100, "center": 50},
             )
+
+    return doc
+
+
+def edit_dispersion_tables(doc, disp):
+    for maturity in [10, 30]:
+        with doc.start_edit(f"dispersion_{maturity}y"):
+            doc.add_text(f"\\Huge \\textbf \u007b{maturity} Year\u007d")
+            overview_table = disp.overview_table(maturity=maturity)
+            doc.add_table(
+                overview_table,
+                adjust=True,
+                caption="Dispersion Overview \\%tiles",
+                prec={col: "0f" for col in overview_table.columns},
+                gradient_cell_col=overview_table.columns,
+                gradient_cell_kws={"vmin": 0, "vmax": 100, "center": 50},
+            )
+
+            intra_sector_table = disp.intra_sector_table(maturity=maturity)
+            doc.add_table(
+                intra_sector_table,
+                adjust=True,
+                caption="Intra-Sector Dispersion \\%tiles",
+                col_fmt="l|rrr|rrr",
+                multi_row_header=True,
+                prec={col: "0f" for col in intra_sector_table.columns},
+                gradient_cell_col=[
+                    col for col in intra_sector_table.columns if "Rel" in col
+                ],
+                gradient_cell_kws={"vmin": 0, "vmax": 100, "center": 50},
+            )
+
     return doc
 
 
 if __name__ == "__main__":
+    db = Database()
+    disp = Dispersion("IG", db)
+    disp.update()
     fid = "Dispersion"
-    maturity = 10
-    update_dispersion(fid)
+    build_dispersion_sheet(fid, disp)
