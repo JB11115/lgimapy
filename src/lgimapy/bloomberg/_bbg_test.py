4feab8341621ae5651abedee44d6d34515d9a4b3
JB11115 <jason.becker@lgima.com>
2022-01-18 14:17:15 -0600

    Added bloomberg api test script

---
+++ src/lgimapy/bloomberg/_bbg_test.py
@@ -0,0 +1,20 @@
+from lgimapy.bloomberg import bdp, bdh, bds
+
+# %%
+
+def main():
+    cusip = '097023BV6'
+    yellow_key = 'Corp'
+    ovrd = {"USER_LOCAL_TRADE_DATE": "19501010"}
+
+    print('testing bdp()...')
+    print(bdp(cusip, yellow_key, 'PX_LAST'))
+
+    print('testing bdh()...')
+    print(bdp(cusip, yellow_key, 'PX_LAST', start='1/1/2022', end='1/10/2022'))
+
+    print('testing bds()...')
+    print(bds(cusip, yellow_key, 'DES_CASH_FLOW', ovrd=ovrd))
+
+if __name__ == '__main__':
+    main()
