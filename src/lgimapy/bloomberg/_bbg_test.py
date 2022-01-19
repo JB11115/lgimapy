00818ca53199d359702956d8635758cf1a500d28
JB11115 <jason.becker@lgima.com>
2022-01-18 18:26:03 -0600

    added newlines between tests

--- src/lgimapy/bloomberg/_bbg_test.py
+++ src/lgimapy/bloomberg/_bbg_test.py
@@ -8,13 +8,13 @@ def main():
     yellow_key = "Corp"
     ovrd = {"USER_LOCAL_TRADE_DATE": "19501010"}
 
-    print("testing bdp()...")
+    print("\ntesting bdp()...")
     print(bdp(cusip, yellow_key, "PX_LAST"))
 
-    print("testing bdh()...")
+    print("\ntesting bdh()...")
     print(bdh(cusip, yellow_key, "PX_LAST", start="1/1/2022", end="1/10/2022"))
 
-    print("testing bds()...")
+    print("\ntesting bds()...")
     print(bds(cusip, yellow_key, "DES_CASH_FLOW", ovrd=ovrd))
 
 
