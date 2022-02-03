4f306cbcee68e8e240d735dc539fb284997a83d4
Jason Becker <jason.becker@lgima.com>
2022-02-03 08:16:36 -0600

    Initial commit for module allowing easy adding of new utility issuers to the utility_buisiness_structure.json data flie.

--- src/lgimapy/data/add_utility_business_structure.py
+++ src/lgimapy/data/add_utility_business_structure.py
@@ -4,24 +4,29 @@ from lgimapy.utils import load_json, dump_json
 
 # %%
 
-fid = "utility_business_structure"
-new_issuer = input("\nEnter Issuer Name:\n").strip()
-options = {
-    0: "HOLDCO",
-    1: "OPCO",
-    2: np.nan,
-    3: 'cancel',
-}
-print(f"\nSelect the option for the utility businss structure of")
-print(f"'{new_issuer}':\n")
-for key, val in options.items():
-    print(f"  {key}) {val}")
+def add_utility_business_structure():
+    fid = "utility_business_structure"
+    new_issuer = input("\nEnter Issuer Name:\n").strip()
+    options = {
+        0: "HOLDCO",
+        1: "OPCO",
+        2: np.nan,
+        3: 'cancel',
+    }
+    print(f"\nSelect the option for the utility businss structure of")
+    print(f"'{new_issuer}':\n")
+    for key, val in options.items():
+        print(f"  {key}) {val}")
 
-new_business_structure_key = int(input())
-if new_business_structure_key == 3:
-    quit()
+    new_business_structure_key = int(input())
+    if new_business_structure_key == 3:
+        quit()
 
-old_d = load_json(fid)
-new_row_d = {new_issuer: options[new_business_structure_key]}
-new_d = {**old_d, **new_row_d}
-dump_json(new_d, fid)
+    old_d = load_json(fid)
+    new_row_d = {new_issuer: options[new_business_structure_key]}
+    new_d = {**old_d, **new_row_d}
+    dump_json(new_d, fid)
+
+# %%
+if __name__ == "__main__":
+    add_utility_business_structure()
