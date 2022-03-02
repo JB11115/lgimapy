8ace28de58dc1b432e7c85a1d77e59ea84030edb
Jason Becker <jason.becker@lgima.com>
2022-03-02 12:49:29 -0600

    updated syntax

--- src/lgimapy/data/account_values.py
+++ src/lgimapy/data/account_values.py
@@ -1,10 +1,14 @@
 from lgimapy.data import Database
-from lgimapy.utils import root
 
 
 def update_account_market_values():
     """
     Update account values for each strategy.
     """
-    df = Database().account_values()
-    df.to_parquet(root("data/account_values.parquet"))
+    db = Database()
+    df = db.account_values()
+    df.to_parquet(db.local("account_values.parquet"))
+
+
+if __name__ == "__main__":
+    update_account_market_values()
