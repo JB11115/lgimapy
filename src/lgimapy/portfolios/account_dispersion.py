b92ade9c011d497490fc3eea1223cdda02b03019
Jason Becker <jason.becker@lgima.com>
2022-03-02 12:50:39 -0600

    Initial commit for script to show dispersion in stored portfolio metrics

--- src/lgimapy/portfolios/account_dispersion.py
+++ src/lgimapy/portfolios/account_dispersion.py
@@ -6,13 +6,12 @@ from lgimapy.data import Database, Portfolio
 def get_account_dispersion(strategy, date=None):
     db = Database()
     if date is None:
-        date = db.date('today')
+        date = db.date("today")
     accounts = db._strategy_to_accounts[strategy]
     df_list = []
     for account in accounts:
-        port = Portfolio(date, name=account, class_name='Account')
+        port = Portfolio(date, name=account, class_name="Account")
         df_list.append(port.stored_properties_history_df.loc[date])
-
 
     return pd.DataFrame(df_list, index=accounts)
 
@@ -25,5 +24,3 @@ def main():
     df = get_account_dispersion(strategy)
     strat = Portfolio(date, name=strategy, class_name="Strategy")
     df.to_csv(f"{strat.fid}_account_dispersion.csv")
-
-    df['tracking_error'] / df['']
