ef4b9acd9062ff1fad2e02c62b5a327f34fffd55
Jason Becker <jason.becker@lgima.com>
2022-03-04 06:50:59 -0600

    Added strategy risk report and cahnged function names to build_...

--- src/lgimapy/daily_scripts/__init__.py
+++ src/lgimapy/daily_scripts/__init__.py
@@ -2,7 +2,7 @@ from lgimapy.daily_scripts.issuer_change_report import (
     build_issuer_change_report,
 )
 from lgimapy.daily_scripts.credit_snapshot import (
-    make_credit_snapshots,
+    build_credit_snapshots,
     update_credit_snapshots,
 )
 from lgimapy.daily_scripts.on_the_run_snapshot import (
@@ -11,13 +11,17 @@ from lgimapy.daily_scripts.on_the_run_snapshot import (
 from lgimapy.daily_scripts.month_end_extension import (
     build_month_end_extensions_report,
 )
-from lgimapy.daily_scripts.sector_report import create_sector_report
+from lgimapy.daily_scripts.sector_report import build_sector_report
+from lgimapy.daily_scripts.strategy_risk_report import (
+    build_strategy_risk_report,
+)
 
 __all__ = [
     "build_issuer_change_report",
-    "make_credit_snapshots",
+    "build_credit_snapshots",
     "update_credit_snapshots",
     "build_on_the_run_ticker_snapshot",
     "build_month_end_extensions_report",
-    "create_sector_report",
+    "build_sector_report",
+    "build_strategy_risk_report",
 ]
