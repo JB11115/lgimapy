7a777b044878909c6c25e91bc7abf2b113e52b6c
Jason Becker <jason.becker@lgima.com>
2022-04-28 13:07:01 -0500

    Updated syntax for new empty portfolios

--- src/lgimapy/portfolios/account_dispersion.py
+++ src/lgimapy/portfolios/account_dispersion.py
@@ -7,10 +7,10 @@ def get_account_dispersion(strategy, date=None):
     db = Database()
     if date is None:
         date = db.date("today")
-    accounts = db._strategy_to_accounts[strategy]
+    accounts = db._account_strategy_map(date).keys()
     df_list = []
     for account in accounts:
-        port = Portfolio(date, name=account, class_name="Account")
+        port = db.load_portfolio(date, account=account, empty=True)
         df_list.append(port.stored_properties_history_df.loc[date])
 
     return pd.DataFrame(df_list, index=accounts)
@@ -22,5 +22,5 @@ def main():
     strategy = "US Long Credit"
     date = None
     df = get_account_dispersion(strategy)
-    strat = Portfolio(date, name=strategy, class_name="Strategy")
+    strat = db.load_portfolio(date, strateg=strategy, empty=True)
     df.to_csv(f"{strat.fid}_account_dispersion.csv")
