d21586e25f45f12a61c9108cb19ee1ce7f2ec8c1
Jason Becker <jason.becker@lgima.com>
2022-04-28 13:07:34 -0500

    Added exception clause required in pandas=1.4.1

--- src/lgimapy/stats/stats.py
+++ src/lgimapy/stats/stats.py
@@ -1,4 +1,6 @@
 import warnings
+
+import numpy as np
 from scipy import stats
 
 
@@ -6,4 +8,7 @@ def mode(x):
     """Get most frequent occurance in Pandas aggregate by."""
     with warnings.catch_warnings():
         warnings.simplefilter("ignore", category=RuntimeWarning)
-        return stats.mode(x)[0][0]
+        try:
+            return stats.mode(x)[0][0]
+        except IndexError:
+            return np.nan
