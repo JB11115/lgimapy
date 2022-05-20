3f360daf336d46601762987df88001d8d7ca6ac1
Jason Becker <jason.becker@lgima.com>
2022-05-20 12:42:09 -0500

    Changed function name

--- src/lgimapy/HY/__main__.py
+++ src/lgimapy/HY/__main__.py
@@ -9,9 +9,10 @@ from lgimapy.utils import root
 
 from cover_page import update_cover_page
 from rating_performance import update_rating_performance
-from spreads_yields_returns_report import update_spreads_yields_returns
+from decile_report import build_decile_report
 from sector_comp import update_sector_models
 from valuations_issuer_mv_weighted import update_valuations
+from sector_dispersion import update_sector_dispersion
 from dispersion import update_HY_spread_dispersion
 from default_rates import update_default_rate_pdf
 from fallen_angels_rising_stars import update_fallen_angels_rising_stars
@@ -30,9 +31,10 @@ def main():
     funcs = {
         update_cover_page: ("HY_Cover_Page", db),
         update_rating_performance: "Rating_Performance",
-        update_spreads_yields_returns: "HY_spreads_yields_returns",
+        build_decile_report: "HY_spreads_yields_returns",
         update_sector_models: ("Sector_Valuations", db),
         update_valuations: ("HY_Valuations", db),
+        update_sector_dispersion: "HY_Sector_Dispersion",
         update_HY_spread_dispersion: "HY_Spread_Dispersion",
         update_default_rate_pdf: "Default_Rates",
         update_fallen_angels_rising_stars: "Potential_Fallen_Angels_Rising_Stars",
