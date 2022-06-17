5a3029a0cbcde5475b46ff92451b96cc6ee7068b
Jason Becker <jason.becker@lgima.com>
2022-06-17 14:34:25 -0500

    used black to fix code

--- src/lgimapy/portfolios/wisayah.py
+++ src/lgimapy/portfolios/wisayah.py
@@ -1,13 +1,18 @@
+from lgimapy.data import Database
 from lgimapy.portfolios import AttributionIndex
 
-account = 'SALD'
+
+account = "SALD"
+db = Database()
 attr = AttributionIndex(account, start=db.date("WTD"))
 
-print('\n\nTickers')
+print(f"\n\nTotal: {attr.total():.1f}")
+
+print("\n\nTickers")
 print(attr.best_worst_df(attr.tickers()))
 
-print('\n\nSectors')
+print("\n\nSectors")
 print(attr.best_worst_df(attr.sectors()))
 
-print('\n\nMarket Segments')
+print("\n\nMarket Segments")
 print(attr.best_worst_df(attr.market_segments()))
