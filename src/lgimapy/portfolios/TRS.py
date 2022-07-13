70463387416db19a9040c3cecfdef599a85aa6b0
Jason Becker <jason.becker@lgima.com>
2022-07-13 10:12:24 -0500

    Added performance calculation

--- src/lgimapy/portfolios/TRS.py
+++ src/lgimapy/portfolios/TRS.py
@@ -8,12 +8,10 @@ from lgimapy.data import Database
 
 
 db = Database()
-start = "5/16/2022"
-end = '5/20/2022'
+start = "6/8/2022"
+end = None
 db.load_market_data(start=start, end=end)
 
-port = db.load_portfolio(account='LIB350', date=start)
-port.derivatives_df.iloc[0]['P_Notional']
 
 # %%
 dates = db.trade_dates(start=start, end=end)
@@ -25,15 +23,15 @@ for date in dates:
         "INDX_MWEIGHT_HIST",
         ovrd={"END_DATE_OVERRIDE": fmt_bbg_dt(date)},
     )
-    df.columns = ['BBG_ID', 'Weight']
-    df['ISIN'] = id_to_isin(df['BBG_ID'])
-    index_weights = dict(zip(df['ISIN'], df['Weight'] / 100))
+    df.columns = ["BBG_ID", "Weight"]
+    df["ISIN"] = id_to_isin(df["BBG_ID"])
+    index_weights = dict(zip(df["ISIN"], df["Weight"] / 100))
     ix = db.build_market_index(date=date, isin=index_weights.keys())
-    ix.df['P_Weight'] = -ix.df['ISIN'].map(index_weights)
-    ix.df['BM_Weight'] = 0
+    ix.df["P_Weight"] = -ix.df["ISIN"].map(index_weights)
+    ix.df["BM_Weight"] = 0
 
     port_tret = (ix.df["TRet"] * ix.df["P_Weight"]).sum()
-    bm_tret =  (ix.df["TRet"] * ix.df["BM_Weight"]).sum()
+    bm_tret = (ix.df["TRet"] * ix.df["BM_Weight"]).sum()
     tret = port_tret - bm_tret
     tret_ts_list.append(tret)
 
@@ -41,4 +39,4 @@ tret_ts = pd.Series(tret_ts_list, index=dates)
 tret_ts
 
 # %%
-1e4* (np.prod(1 + tret_ts) - 1)
+1e4 * (np.prod(1 + tret_ts) - 1)
