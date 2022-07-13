e1e207467c04b1646f1d98cc581836f772471a6c
JB11115 <jason.becker@lgima.com>
2022-07-13 14:59:16 -0500

    cleanup up main()

--- src/lgimapy/tea.py
+++ src/lgimapy/tea.py
@@ -7,16 +7,22 @@ from time import sleep
 
 from lgimapy.utils import beep
 
+
 def parse_args():
     """Collect settings from command line and set defaults."""
     parser = argparse.ArgumentParser()
-    parser.add_argument('-m', "--minutes", help="Minutes before beep")
-    parser.set_defaults(minutes='3.5')
+    parser.add_argument("-m", "--minutes", help="Minutes before beep")
+    parser.set_defaults(minutes="3.5")
     return parser.parse_args()
 
-if __name__ == '__main__':
+
+def beep_when_tea_is_done_steeping():
     args = parse_args()
     minutes = float(args.minutes)
     seconds = minutes
     sleep(seconds)
     beep()
+
+
+if __name__ == "__main__":
+    beep_when_tea_is_done_steeping()
