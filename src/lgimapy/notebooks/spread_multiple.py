c7efeb8cff832ab635191091a65018a31654ddbb
Jason Becker <jason.becker@lgima.com>
2022-07-13 10:37:37 -0500

    updated notebooks

--- src/lgimapy/notebooks/spread_multiple.py
+++ src/lgimapy/notebooks/spread_multiple.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import seaborn as sns
 
 from lgimapy import vis
@@ -14,7 +15,7 @@ colors = None
 beta_adjusted = True
 
 
-def plot_spread_multiple(
+def plot_spread_return(
     cols, fid, start=None, colors=None, end=None, beta_adjusted=False
 ):
     db = Database()
@@ -23,10 +24,8 @@ def plot_spread_multiple(
     oas_df = db.load_bbg_data(cols, "OAS", start=start, end=end).fillna(
         method="ffill"
     )
-    delta_oas_df = (oas_df.iloc[-1, :] - oas_df.iloc[0, :]) / (
-        oas_df.iloc[0, :]
-    )
-
+    delta_oas_df = oas_df - oas_df.iloc[0, :]
+    spread_return_df = -delta_oas_df / oas_df.iloc[0, :]
     if beta_adjusted:
         oad_df = db.load_bbg_data(cols, "OAS", start=start, end=end).fillna(
             method="ffill"
@@ -36,10 +35,23 @@ def plot_spread_multiple(
         )
         dts_df = oas_df * oad_df
         mv_dts = (mv_df * dts_df).iloc[0, :]
-        beta = mv_dts / mv_dts.mean()
+        beta_s = mv_dts / mv_dts.mean()
 
+        mean_delta_oas = (mv_df * delta_oas_df).sum(axis=1) / mv_df.sum(axis=1)
+        beta_fcast_delta_oas_df = pd.concat(
+            (
+                (mean_delta_oas * beta).rename(col)
+                for col, beta in beta_s.items()
+            ),
+            axis=1,
+        )
+        beta_adjusted_delta_oas_df = delta_oas_df - beta_fcast_delta_oas_df
+        beta_adjusted_spread_return_df = (
+            -beta_adjusted_delta_oas_df / oas_df.iloc[0, :]
+        )
+        df = beta_adjusted_spread_return_df.copy()
     else:
-        df = delta_oas_df.copy()
+        df = spread_return_df.copy()
     df.columns = db.bbg_names(df.columns)
 
     fig, ax = vis.subplots(figsize=(12, 8))
@@ -52,20 +64,27 @@ def plot_spread_multiple(
             df[col],
             color=color,
             alpha=0.7,
-            label=f"{col}: {val:.2f}",
-            ylabel="Spread Multiple",
+            label=f"{col}: {val:.0%}",
+            ylabel="Spread Return",
+            ytickfmt="{x:.0%}",
             ax=ax,
         )
     vis.legend(ax)
     vis.savefig(fid)
 
 
-plot_spread_multiple(
+plot_spread_return(
     ["US_IG_10+", "US_IG", "US_HY", "EU_IG", "GBP_IG"],
-    fid="2022_Index_Spread_YTD_multiple",
+    fid="2022_Index_Spread_Return_YTD",
 )
 
-plot_spread_multiple(
+plot_spread_return(
     ["US_AAA", "US_AA", "US_A", "US_BBB", "US_BB", "US_B", "US_CCC"],
-    fid="2022_Rating_Spread_YTD_multiple",
+    fid="2022_Rating_Spread_Return_YTD",
+)
+
+plot_spread_return(
+    ["US_AAA", "US_AA", "US_A", "US_BBB", "US_BB", "US_B", "US_CCC"],
+    beta_adjusted=True,
+    fid="2022_Rating_Beta_Adjusted_Spread_Return_YTD",
 )
